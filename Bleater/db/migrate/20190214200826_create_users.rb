a9062d9fcc0609b63b07c5623f463284285b27d5
web615 <=>
2019-02-14 15:53:52 -0500

    readding assignment 1 and 2

---
+++ Bleater/db/migrate/20190214200826_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :string
+      t.string :last_name
+      t.string :email
+      t.string :street_address
+      t.string :city
+      t.string :province
+      t.string :postal_code
+      t.string :country
+
+      t.timestamps
+    end
+  end
+end
