e98b70b7ea7f271c7ac76a4432aee3f2376b3325
web615 <=>
2019-02-14 22:47:02 -0500

    finished third lab

--- Bleater/db/seeds.rb
+++ Bleater/db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+24.times do
+
+  user  = User.create([{
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      email: Faker::Internet.email,
+      street_address: Faker::Address.street_address,
+      city: Faker::Address.city,
+      province: Faker::Address.state,
+      postal_code: Faker::Address.zip,
+      country: Faker::Address.country
+               }])
+
+      24.times do
+        Micropost.create([{
+            message: Faker::Movies::LordOfTheRings.quote,
+            user: user[0]
+                          }])
+      end
+end
